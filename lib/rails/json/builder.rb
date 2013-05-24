2cd405319de9f65bcf55ff38fe15747f5cd9d368
Alex Berry <highwaybobbery@gmail.com>
2013-05-24 12:27:38 -0400

    initial commit

---
+++ lib/rails/json/builder.rb
@@ -0,0 +1,9 @@
+require "rails/json/builder/version"
+
+module Rails
+  module Json
+    module Builder
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rails/json/builder/version.rb
@@ -0,0 +1,7 @@
+module Rails
+  module Json
+    module Builder
+      VERSION = "0.0.1"
+    end
+  end
+end
